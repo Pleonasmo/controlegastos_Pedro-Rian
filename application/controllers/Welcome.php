3bceb7bce8ef10a4aa6eddb1a1be98ab035dc060
Pleonasmo <pepirian24@gmail.com>
2021-02-22 01:30:48 -0300

    Criação de view novo_gasto.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,6 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('listar_gastos');
+		$this->load->view('novo_gasto');
 	}
 }
---
+++ application/views/novo_gasto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+
+        <input type="date" name="data" id="">
+        <input type="text" name="descricao" id="">
+        <input type="text" name="valor" id="">
+        <button type="submit">Confirmar</button>
+        <button>Limpar</button>
+
+    </form>
+</body>
+</html>
