dc24f062405cf78960bfa0e33abde0351c64e4ef
Pleonasmo <pepirian24@gmail.com>
2021-02-22 01:18:16 -0300

    criação de tabela de gastos

---
+++ application/views/listar_gastos.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela de gastos</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>Data</td>
+            <td>Descrição</td>
+            <td>Valor</td>
+        </tr>
+        <tr>
+            <td>10/01/2021</td>
+            <td>Material Escolar</td>
+            <td>65.00</td>
+        </tr>
+        <tr>
+            <td>02/02/2021</td>
+            <td>Curso Online</td>
+            <td>150.00</td>
+        </tr>
+        <tr>
+            <td>21/02/2021</td>
+            <td>Livro de Lógica</td>
+            <td>30.00</td>
+        </tr>
+        <tr>
+            <td>05/03/2021</td>
+            <td>Teclado mecânico</td>
+            <td>230.00</td>
+        </tr>
+    </table>
+</body>
+</html>
